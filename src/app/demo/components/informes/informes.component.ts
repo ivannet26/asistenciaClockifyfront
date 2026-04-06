de80e032cb30a576a8c4936eec02a8ca39dc2aa1
Rick <sernaquericardo10@gmail.com>
2026-04-06 17:10:18 -0500

    Avance de Informe, Panel y calendario completos,menu layout fixed y eliminacion de archivos innecesarios

--- src/app/demo/components/informes/informes.component.ts
+++ src/app/demo/components/informes/informes.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { TabMenuModule } from "primeng/tabmenu";
+import { DropdownModule } from "primeng/dropdown";
+import { CalendarModule } from "primeng/calendar";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-informes',
   standalone: true,
-  imports: [],
+  imports: [TabMenuModule, DropdownModule, CalendarModule, TableModule,FormsModule,CommonModule],
   templateUrl: './informes.component.html',
   styleUrl: './informes.component.css'
 })
 export class InformesComponent {
+  selectedDate: Date = new Date();
+  onDateSelect($event: any) {
+    throw new Error('Method not implemented.');
+  }
+  tabs = [
+    { label: 'Resumido', id: 0 },
+    { label: 'Detallado', id: 1 },
+    { label: 'Semanal', id: 2 },
+    { label: 'Compartido', id: 3 }
+  ];
+
+    equipos = [
+    { nombre: 'miembro equipo', 
+      correo: 'miembro@equipo.com', 
+      tarifa: '$50/hora', 
+      tarifa_coste: '$30/hora', 
+      rol: 'propietario', 
+      grupo:'Grupo 1' }
+  ];
+
+  grupos = [
+    { nombre: 'Grupo 1', acceso: 'miembro equipo' },
+  ]
+
+  opcionesFiltro = [
+    { label: 'Mostrar activo', value: 'activo' },
+    { label: 'Mostrar inactivos', value: 'inactivo' }
+  ];
+
+  tabactivo = this.tabs[0];
+
+  CambioTab(event: any) {
+    this.tabactivo = event;
+  }
 
 }
